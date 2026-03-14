51789977534b1a88c0469538f34f4c2280f52500
antoine <antoine.weisse@epitech.eu>
2026-03-14 18:29:42 -0400

    [fix] little graphique change

--- src/components/editor/IconDisplayer.tsx
+++ src/components/editor/IconDisplayer.tsx
@@ -1,25 +1,29 @@
-import { FaRegCircle, FaCube } from 'react-icons/fa';
+import { FaRegCircle, FaCube } from "react-icons/fa";
 import { BsCameraVideoFill } from "react-icons/bs";
 import { MdLightMode } from "react-icons/md";
 import { MdOutlineRectangle } from "react-icons/md";
 
-
-
-export default function IconDisplayer({ type,size = 12 } : { type: string, size?: number }) {
-    const displayIcon = () => {
-        switch(type) {
-            case "camera":
-                return <BsCameraVideoFill size={size} />;
-            case "light":
-                return <MdLightMode size={size} />;
-            case "Rectangle":
-                return <MdOutlineRectangle size={size} />;
-            case "Circle":
-                return <FaRegCircle size={size} />;
-            default:
-                return <FaCube size={size} />;
-        }
+export default function IconDisplayer({
+  type,
+  size = 12,
+}: {
+  type: string;
+  size?: number;
+}) {
+  const displayIcon = () => {
+    switch (type) {
+      case "camera":
+        return <BsCameraVideoFill size={size} />;
+      case "light":
+        return <MdLightMode size={size} />;
+      case "Rectangle":
+        return <MdOutlineRectangle size={size} />;
+      case "Circle":
+        return <FaRegCircle size={size} />;
+      default:
+        return <FaCube size={size} />;
     }
+  };
 
-    return displayIcon();
+  return displayIcon();
 }
--- src/components/editor/Toolbar.tsx
+++ src/components/editor/Toolbar.tsx
@@ -1,55 +1,46 @@
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-
-import { Label } from "../ui/label";
 import { Button } from "../ui/button";
-
+import { IoClose } from "react-icons/io5";
+import { FaPlus } from "react-icons/fa";
 import { FaPlay } from "react-icons/fa";
 import { FaStop } from "react-icons/fa";
 import { FaPause } from "react-icons/fa";
 
-export function Toolbar() {
-    const scenes = ["SampleScene", "AnotherScene"];
-    return (
-        <div className="w-full h-full flex flex-row items-center justify-between bg-primary/10">
-            <div className="flex flex-row px-2 py-1 my-1 bg-primary/10 items-center gap-2 ml-4 rounded-md">
-                <Label className="font-semibold bg-">
-                    Actual Scene:
-                </Label>
-                <Select defaultValue="SampleScene">
-                    <SelectTrigger className="w-[150px] border-none bg-secondary/50 focus:ring-0 focus:ring-offset-0">
-                        <SelectValue placeholder="Select Scene" />
-                    </SelectTrigger>
-                    <SelectContent 
-                        side="bottom"
-                        align="start"
-                        position="popper"
-                        sideOffset={-10}
-                    >
-                        {scenes.map((scene) => (
-                            <SelectItem key={scene} value={scene}>
-                                {scene}
-                            </SelectItem>
-                        ))}
-                    </SelectContent>
-                </Select>
-            </div>
-            <div className="mr-2 flex flex-row items-center">
-                <Button variant="ghost" className="cursor-pointer" size={"icon-sm"}>
-                    <FaPlay className="ml-0.5" />
-                </Button>
-                <Button variant="ghost" className="cursor-pointer" size={"icon-sm"}>
-                    <FaPause />
-                </Button>
-                <Button variant="ghost" className="cursor-pointer" size={"icon-sm"}>
-                    <FaStop  />
-                </Button>
-            </div>
+function ScenesDisplayer() {
+  const scenes = ["SampleScene", "AnotherScene"];
+
+  return (
+    <div className="w-full flex items-end justify-start h-8">
+      {scenes.map((scene) => (
+        <div
+          key={scene}
+          className="bg-primary/5 px-2 py-1 border-r border-primary/10 flex items-center gap-1 h-full"
+        >
+          <span>{scene}</span>
+          <IoClose className="cursor-pointer" size={20} color="red" />
         </div>
-    );
+      ))}
+      <div className="bg-primary/5 px-2 py-1 border-r border-primary/10 flex items-center gap-1 h-full">
+        <FaPlus className="cursor-pointer" size={16} />
+      </div>
+    </div>
+  );
+}
+
+export function Toolbar() {
+  return (
+    <div className="w-full h-full flex flex-row justify-between bg-primary/10 items-end">
+      <ScenesDisplayer />
+      <div className="flex flex-row items-center bg-primary/10 gap-1 h-10 m-1 rounded-md px-2">
+        <Button variant="ghost" className="cursor-pointer" size={"icon-sm"}>
+          <FaPlay className="ml-0.5" />
+        </Button>
+        <Button variant="ghost" className="cursor-pointer" size={"icon-sm"}>
+          <FaPause />
+        </Button>
+        <Button variant="ghost" className="cursor-pointer" size={"icon-sm"}>
+          <FaStop />
+        </Button>
+      </div>
+    </div>
+  );
 }
