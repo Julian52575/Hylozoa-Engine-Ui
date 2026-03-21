76d020ee38371cc7748fb74eeec8fda164d45014
antoine <antoine.weisse@epitech.eu>
2026-03-20 20:54:45 -0400

    [add] bouton play run binary and button stop stop it

--- src/components/editor/Toolbar.tsx
+++ src/components/editor/Toolbar.tsx
@@ -1,23 +1,59 @@
 import { Button } from "../ui/button";
-import { FaPlay } from "react-icons/fa";
-import { FaStop } from "react-icons/fa";
-import { FaPause } from "react-icons/fa";
+import { FaPlay, FaSpinner, FaStop } from "react-icons/fa";
+import { Command, Child } from "@tauri-apps/plugin-shell";
+import { useState, useRef } from "react";
 
 export function Toolbar() {
-    return (
-        <div className="w-full h-12 bg-primary/10 flex items-center px-2 gap-2 justify-center border-b border-primary/20">
-            <Button variant="outline" className="cursor-pointer">
-                <FaPlay className="ml-0.5" />
-                <span>Play</span>
-            </Button>
-            <Button variant="outline" className="cursor-pointer">
-                <FaPause />
-                <span>Pause</span>
-            </Button>
-            <Button variant="outline" className="cursor-pointer">
-                <FaStop />
-                <span>Stop</span>
-            </Button>
-        </div>
-    );
+  const [status, setStatus] = useState<"idle" | "running">("idle");
+  const childRef = useRef<Child | null>(null);
+
+  async function launchHylozoa() {
+    if (status === "running") return;
+
+    const command = Command.sidecar("binaries/hylozoa", ["mon-argument"]);
+    const child = await command.spawn();
+    childRef.current = child;
+
+    setStatus("running");
+
+    command.on("close", () => {
+      setStatus("idle");
+      childRef.current = null;
+    });
+  }
+
+  async function handleStop() {
+    if (childRef.current) {
+      await childRef.current.kill();
+      setStatus("idle");
+      childRef.current = null;
+    }
+  }
+
+  return (
+    <div className="w-full h-12 bg-primary/10 flex items-center px-2 gap-2 justify-center border-b border-primary/20">
+      <Button
+        variant="outline"
+        className="cursor-pointer"
+        onClick={launchHylozoa}
+        disabled={status === "running"}
+      >
+        {status === "running" ? (
+          <FaSpinner className="animate-spin mr-0.5" />
+        ) : (
+          <FaPlay className="ml-0.5" />
+        )}
+        <span>Play</span>
+      </Button>
+      <Button 
+        variant="outline" 
+        className="cursor-pointer active:scale-95 " 
+        onClick={handleStop}
+        disabled={status === "idle"}
+    >
+        <FaStop />
+        <span>Stop</span>
+      </Button>
+    </div>
+  );
 }
